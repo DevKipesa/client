8717e444905dbc75005c5984681e45127eb9110a
Dominic Kimani <kymdommie@gmail.com>
2023-11-15 16:16:49 +0300

    few changes

---
+++ src/components/Home/home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Navbar from '../navbar/navbar';
+function home() {
+  return (
+    <div>
+        <Navbar/>
+    </div>
+  )
+}
+
+export default home
---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './navbar.scss';
+function navbar() {
+  return (
+    <div>
+        <div className='nav'>
+            <div className='nation'>National</div>
+         <div className='count'>County</div>
+         <div className='sub'>Sub-County</div>
+         
+        </div>
+    </div>
+  )
+}
+
+export default navbar
