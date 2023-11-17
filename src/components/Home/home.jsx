0594586acd712e93dc4ca774c91ef53be0a6ceef
Dominic Kimani <kymdommie@gmail.com>
2023-11-17 10:12:32 +0300

    few changes

--- src/components/Home/home.jsx
+++ src/components/Home/home.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Navbar from '../navbar/navbar';
 import Dashboard from '../Dashboard/dashboard';
 import Card from '../card/card';
+import Card1 from '../card/card1';
 function home() {
   return (
     <div>
         <Navbar/>
         <Dashboard/>
         <Card/>
+        <Card1 />
     </div>
   )
 }
---
+++ src/components/card/card1.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './card1.scss';
+function card1() {
+
+  return (
+    <div>
+      <div className='funds'>
+      <div className='ved'>
+        <h3>Funds</h3>
+        <p> Allocated 1B <br/>
+        Disbursed   500m</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default card1
